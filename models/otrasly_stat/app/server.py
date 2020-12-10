d6cf86dae26d36538c903e41fbb13fc56cd3644a
n00bcracker <zacpa@ya.ru>
2020-12-10 18:12:15 +0300

    Add server_routes to server

--- models/otrasly_stat/app/server.py
+++ models/otrasly_stat/app/server.py
@@ -1,14 +1,10 @@
 from flask import Flask
-from rq import Queue
-from redis import Redis
 from models.otrasly_stat import OtrasliStat
 
 app = Flask(__name__)
 app.logger.setLevel("DEBUG")
 
-redis_conn = Redis(host='redis')
-queue = Queue(connection=redis_conn)
 model = OtrasliStat()
 
-
+from models.otrasly_stat.app import server_routes
 app.logger.info("Otrasly Stat service startup")
--- models/otrasly_stat/config_otrasli.py
+++ models/otrasly_stat/config_otrasli.py
@@ -1,3 +1,3 @@
 import os
 # Названия таблиц
-OTRASLI_TABLE = os.getenv('OTRASLI_TABLE') or 'si_otrasli_stat'
+OTRASLI_TABLE = os.getenv('OTRASLI_TABLE') or None
