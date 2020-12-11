c3c0072223abf56f373a0aedcbe0ff0d826d171c
Ticket 016240356 <zaspa_ay@app-cfdvmd01n01.open.ru>
2020-12-11 17:08:00 +0300

    Change output and okved column name

--- models/otrasly_stat/otrasli_stat.py
+++ models/otrasly_stat/otrasli_stat.py
@@ -2,6 +2,7 @@ from models.otrasly_stat.config_otrasli import OTRASLI_TABLE
 from utils import resources
 from common import OracleDB
 import re
+import numpy as np
 #import pandas as pd
 #import sqlalchemy as sa
 
@@ -20,7 +21,7 @@ class OtrasliStat(OracleDB):
         sql_query = """                                                  
                        select * 
                        from {tablename} t 
-                       where 1=1 and t.grp = :okved
+                       where 1=1 and t.okved = :okved
                     """
         sql_query = sql_query.format(tablename=self.otrasli_table_name)
         otrasl_df = self.read_sql_query(sql_query, params=[okved, ])
@@ -47,21 +48,11 @@ class OtrasliStat(OracleDB):
                  res[resources.RESPONSE_STATUS_FIELD] = 'Error'
                  res[resources.RESPONSE_ERROR_FIELD] = 'Отсутствуют данные для переданного ОКВЭД'
              elif otrasly_df.shape[0] == 1:
-                  res[resources.RESPONSE_STATUS_FIELD] = 'Ok'
-                  res['okved'] = otrasly_df.grp[0]
-                  res['okved_name'] = otrasly_df.ent_to_gr[0]
-                  res['inn_count'] = otrasly_df.inn_count[0]
-                  res['inn_count_change'] = otrasly_df.inn_count_change[0]
-                  res['new_inn_count'] = otrasly_df.new_inn_count[0]
-                  res['new_inn_count_percent'] = otrasly_df.new_inn_count_percent[0]
-                  res['delete_inn_count'] = otrasly_df.delete_inn_count[0]
-                  res['delete_inn_count_percent'] = otrasly_df.delete_inn_count_percent[0]
-                  res['individual_share'] = otrasly_df.individual_share[0]
-                  res['individual_share_change'] = otrasly_df.individual_share_change[0]
-                  res['less_business_region'] = otrasly_df.less_business_region[0]
-                  res['less_business_region_share'] = otrasly_df.less_business_region_share[0]
-                  res['most_business_region'] = otrasly_df.most_business_region[0]
-                  res['most_business_region_share'] = otrasly_df.most_business_region_share[0]
+                  res[resources.RESPONSE_STATUS_FIELD] = 'Ok'               
+                  for i in otrasly_df.columns[otrasly_df.isna().any()==False].tolist()[1:]:
+                        if otrasly_df[i].dtypes == 'int64': res[i] = float(otrasly_df[i][0])
+                        else: res[i] = otrasly_df[i][0]
+                  
              elif otrasly_df.shape[0] > 1:
                   res[resources.RESPONSE_STATUS_FIELD] = 'Error'
                   res[resources.RESPONSE_ERROR_FIELD] = 'Переданный ОКВЭД дублируется'
