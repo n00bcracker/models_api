b428cd6f60fe9af301cf6b53187b388974465752
kaktuz <zacpa@ya.ru>
2021-05-14 11:14:43 +0300

    changed tariff names

--- models/advisor/advisor_stat.py
+++ models/advisor/advisor_stat.py
@@ -1,12 +1,13 @@
 from models.advisor.config import ADVISOR_TABLE, ADVISOR_EVENTS_TABLE, ADVISOR_USER_BDAY_TABLE
 from models.advisor.config import CONGRATULATION_CODES, BLOCKING_REASONS, PRODUCT_OFFERS, TARIFFS
-from utils import resources, check_inn, check_kpp
+from utils import resources
 from common import OracleDB
 
 import datetime
 import calendar
 import pandas as pd
 from sqlalchemy import INTEGER, FLOAT, VARCHAR, DATE
+
 
 class AdvisorStat(OracleDB):
     advisor_tablename = ADVISOR_TABLE
@@ -59,7 +60,8 @@ class AdvisorStat(OracleDB):
         pday = today.replace(day=min(today.day, calendar.monthrange(hday.year, today.month)[1]), year=hday.year)
         delta = pday - hday
         if delta.days >= 0:
-            prevday = today.replace(day=min(today.day, calendar.monthrange(hday.year-1, today.month)[1]), year=hday.year-1)
+            prevday = today.replace(day=min(today.day, calendar.monthrange(hday.year - 1, today.month)[1]),
+                                    year=hday.year - 1)
             nextday = pday
             delta1 = hday - prevday
             delta2 = nextday - hday
@@ -71,7 +73,8 @@ class AdvisorStat(OracleDB):
                 clbr_year = today.year
         else:
             prevday = pday
-            nextday = today.replace(day=min(today.day, calendar.monthrange(hday.year+1, today.month)[1]), year=hday.year+1)
+            nextday = today.replace(day=min(today.day, calendar.monthrange(hday.year + 1, today.month)[1]),
+                                    year=hday.year + 1)
             delta1 = hday - prevday
             delta2 = nextday - hday
             if delta1.days <= delta2.days:
@@ -124,10 +127,10 @@ class AdvisorStat(OracleDB):
                     """
 
         adv_sugg_df = self.read_sql_query(sql_query,
-                                            params={
-                                                'org_id' : organization_id,
-                                                'pers_id' : person_id,
-                                            })
+                                          params={
+                                              'org_id': organization_id,
+                                              'pers_id': person_id,
+                                          })
         return adv_sugg_df
 
     def get_user_state(self, organization_id, person_id):
@@ -144,10 +147,10 @@ class AdvisorStat(OracleDB):
                     """
 
         user_state_df = self.read_sql_query(sql_query,
-                                                params={
-                                                    'org_id' : organization_id,
-                                                    'pers_id': person_id,
-                                                })
+                                            params={
+                                                'org_id': organization_id,
+                                                'pers_id': person_id,
+                                            })
         return user_state_df
 
     def update_user_state(self, prev_user_state_df, organization_id, person_id, data):
@@ -158,7 +161,8 @@ class AdvisorStat(OracleDB):
             prev_user_state_df.loc[0, 'organization_id'] = organization_id
             prev_user_state_df.loc[0, 'person_id'] = person_id
 
-            update_success = self.save_df_in_sql_table(prev_user_state_df, self.user_state_dtype, self.advisor_events_tablename)
+            update_success = self.save_df_in_sql_table(prev_user_state_df, self.user_state_dtype,
+                                                       self.advisor_events_tablename)
         else:
             sql_query = """
                             update {tablename}
@@ -185,38 +189,38 @@ class AdvisorStat(OracleDB):
 
         # Модуль поздравлений
         if module_name is None or module_name == 'congratulations':
-            congratulations= list()
+            congratulations = list()
             # День рождения ЛПР
             if pd.notnull(adv_sugg.person_id):
                 days2hday, clbr_year = self.days_to_holiday(adv_sugg.birth_date)
                 prev_clbr_year = user_state.get('birthday')
-                if clbr_year != prev_clbr_year and days2hday >= -3 and days2hday <= 0: # В течение 3х дней после ДР
+                if clbr_year != prev_clbr_year and -3 <= days2hday <= 0:  # В течение 3х дней после ДР
                     congratulations.append({
-                        'code' : 'BIRTHDAY',
-                        'date' : adv_sugg.birth_date.strftime('%Y-%m-%d'),
-                        'celebrationYear' : clbr_year,
+                        'code': 'BIRTHDAY',
+                        'date': adv_sugg.birth_date.strftime('%Y-%m-%d'),
+                        'celebrationYear': clbr_year,
                     })
 
             # День регистрации бизнеса
             if pd.notnull(adv_sugg.company_anniversary):
                 days2hday, clbr_year = self.days_to_holiday(adv_sugg.company_anniversary)
                 prev_clbr_year = user_state.get('company_anniversary')
-                if clbr_year != prev_clbr_year and days2hday >= -5 and days2hday <= 5:
+                if clbr_year != prev_clbr_year and -5 <= days2hday <= 5:
                     congratulations.append({
                         'code': 'COMPANY_ANNIVERSARY',
                         'date': adv_sugg.company_anniversary.strftime('%Y-%m-%d'),
                         'celebrationYear': clbr_year,
-                        'fullYearsFromDate' : int(adv_sugg.company_count_year),
+                        'fullYearsFromDate': int(adv_sugg.company_count_year),
                     })
 
             # День отрасли
             if pd.notnull(adv_sugg.industry_name):
                 days2hday, clbr_year = self.days_to_holiday(adv_sugg.industry_day)
                 prev_clbr_year = user_state.get('industry_day')
-                if clbr_year != prev_clbr_year and days2hday >= -5 and days2hday <= 5:
+                if clbr_year != prev_clbr_year and -5 <= days2hday <= 5:
                     congratulations.append({
                         'code': 'INDUSTRY_DAY',
-                        'indName' : adv_sugg.industry_name,
+                        'indName': adv_sugg.industry_name,
                         'date': adv_sugg.industry_day.strftime('%Y-%m-%d'),
                         'celebrationYear': clbr_year,
                     })
@@ -225,7 +229,7 @@ class AdvisorStat(OracleDB):
             if pd.notnull(adv_sugg.client_anniversary):
                 days2hday, clbr_year = self.days_to_holiday(adv_sugg.client_anniversary)
                 prev_clbr_year = user_state.get('client_anniversary')
-                if clbr_year != prev_clbr_year and days2hday >= -5 and days2hday <= 5:
+                if clbr_year != prev_clbr_year and -5 <= days2hday <= 5:
                     congratulations.append({
                         'code': 'CLIENT_ANNIVERSARY',
                         'date': adv_sugg.client_anniversary.strftime('%Y-%m-%d'),
@@ -253,8 +257,8 @@ class AdvisorStat(OracleDB):
             # Модуль смена тарифа
             if pd.notnull(adv_sugg.change_pu_new) and self.check_tariff_name(adv_sugg.change_pu_new):
                 tariff_change = {
-                    'code' : adv_sugg.change_pu_new,
-                    'profit' : adv_sugg.change_pu_profit,
+                    'code': adv_sugg.change_pu_new,
+                    'profit': adv_sugg.change_pu_profit,
                 }
 
                 offers['tariffChange'] = tariff_change
@@ -283,19 +287,19 @@ class AdvisorStat(OracleDB):
             appeal_themes = list()
             for i in range(1, 4):
                 theme = adv_sugg['themes_appeal_' + str(i)]
-                if pd.notnull(theme) and theme >= 1 and theme <= 33:
+                if pd.notnull(theme) and 1 <= theme <= 33:
                     appeal_themes.append(int(theme))
 
             if len(appeal_themes) > 0:
                 actual_suggestions['information'] = appeal_themes
 
-        if len(actual_suggestions) == 0\
-            or (module_name is None and len(actual_suggestions) == 1\
-                and actual_suggestions.get('sameBusinessChangeValue') is not None):
+        if len(actual_suggestions) == 0 \
+                or (module_name is None and len(actual_suggestions) == 1
+                    and actual_suggestions.get('sameBusinessChangeValue') is not None):
             actual_suggestions = None
 
         return actual_suggestions
-                  
+
     def process_user_feedback(self, organization_id, person_id, congr_code, celebr_year):
         res = dict()
         if self.check_congr_code(congr_code):
@@ -322,7 +326,7 @@ class AdvisorStat(OracleDB):
             user_state_df = self.get_user_state(organization_id, person_id)
             if user_state_df is None:
                 res[resources.RESPONSE_ERROR_FIELD] = 'Ошибка исполнения SQL-запроса'
-            else: # Проверка актуальных предложений
+            else:  # Проверка актуальных предложений
                 act_sugg = self.actual_suggestions_transform(adv_sugg_df, user_state_df)
                 if act_sugg is not None:
                     res['actualSuggestions'] = True
@@ -341,14 +345,13 @@ class AdvisorStat(OracleDB):
         adv_sugg_df = self.get_advisor_suggestions(organization_id, person_id)
         if adv_sugg_df is None:
             res[resources.RESPONSE_ERROR_FIELD] = 'Ошибка исполнения SQL-запроса'
-        elif adv_sugg_df.shape[0] == 1: # Найдены предложения Советника
+        elif adv_sugg_df.shape[0] == 1:  # Найдены предложения Советника
             user_state_df = self.get_user_state(organization_id, person_id)
             if user_state_df is None:
                 res[resources.RESPONSE_ERROR_FIELD] = 'Ошибка исполнения SQL-запроса'
-            else: # Обработка предложений
+            else:  # Обработка предложений
                 act_sugg = self.actual_suggestions_transform(adv_sugg_df, user_state_df, module_name)
                 if act_sugg is not None:
                     res.update(act_sugg)
 
         return res
-    
